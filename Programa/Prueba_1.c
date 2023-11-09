1ec76e848f68f02283b440879f4730aec93114d6
DiwsgenLop <diwsgen.lopez@alumno.buap.mx>
2023-11-09 13:22:15 -0600

    Funcionamiento correcto con salida de cinta final

--- Programa/Prueba_1.c
+++ Programa/Prueba_1.c
@@ -1,10 +1,10 @@
-//Lbreira para la lectura del archivo de texto
+//Libreria para la lectura del archivo de texto
 #include <stdio.h>
 //Para la libreria de las cadenas 
 #include <string.h>
 
 //Cinta con un maximo de 2000 caracteres
-#define max_cinta 2000
+#define cinta_maxima 2000
 //Estructura de la maquina de turing
 typedef struct{
     //Numero de estados
@@ -16,21 +16,19 @@ typedef struct{
 }TablaMT;
 //Definimos la matris de la maquina de turing
 TablaMT MT[100][100];
-//El maximo de la cinta char
-char cinta[max_cinta];
-//Un indice para ver donde va recorriendo en la cinta
-int indice_cinta=0;
-//Declaramos variables para guardar los datos de nuestro archivo
-int No_estado, No_simbolo;
-char Caracteres_cinta[] = "";
-//La entrada del usuario
-char entrada[max_cinta/4] = "";
-//Estados iniciales y finales (int)
-int estado_inicial, estado_final;
-//Tam_in es para saber el tamaño de la entrada del usuario
-int tam_in;
-//Prueba de estado auxiliar (Para funcion busqueda)
-int aux_estado;
+
+//Variables del archivo txt
+int No_estado, No_simbolo, estado_inicial, estado_final, tam_in;//Tam_in es el tamaño de la entrada del usuario
+//Los simbolos de la cinta inicial seran llenados por espacios en blanco
+//Estos espacios seran representados por '_'
+char Caracteres_cinta[100] = "";
+//Entrada del usuario
+char entrada[cinta_maxima/4] = "";
+//Cinta donde se traspasa los datos de entrada y sera cambiada
+char cinta[cinta_maxima];
+//Indice donde apuntara hacia la cinta
+int indice_cinta=cinta_maxima/2; //Tendra espacio hacia adelante o atras
+
 //----------------------------------------------------------------------------------------------
 //Metodo para busqueda de simbolos en la matriz de la maquina de turing
 int busqueda(char simbolo);
@@ -39,7 +37,7 @@ int busqueda(char simbolo);
 int main(){
     //Abrimos el archivo de texto
     FILE *archivo;
-    archivo = fopen("MT.txt","r");
+    archivo = fopen("Lectura.txt","r");
     //Si no se puede abrir el archivo
     if(archivo == NULL){
         printf("No se pudo abrir el archivo");
@@ -48,28 +46,26 @@ int main(){
     else{
         fscanf(archivo,"%d",&No_estado); //Numero de estados totales
         fscanf(archivo,"%d",&No_simbolo); //Numero de simbolos totales
-        fscanf(archivo,"%s",Caracteres_cinta); //Caracteres de la cinta
         fscanf(archivo,"%d",&estado_inicial); //Estado inicial
         fscanf(archivo,"%d",&estado_final); //Estado final
-        //Lemos la entrada del usuario
-        fscanf(archivo,"%s",entrada);
+        fscanf(archivo,"%s",entrada);        //Lemos la entrada del usuario
+        fscanf(archivo,"%s",Caracteres_cinta); //Caracteres de la cinta
         //Lectura de estados, simbolos y direcciones
         for(int i=0;i<No_estado;i++){
             for(int j=0;j<No_simbolo;j++){
-                //Hay un error al momento de leerlo en la matriz, no se porque
-                fscanf(archivo," %d",&MT[i][j].estado);
-                // Leer el espacio en blanco antes del carácter
-                //fgetc(archivo);
-                fscanf(archivo," %c",&MT[i][j].simbolo);
-                // Leer el espacio en blanco después del carácter
-                //fgetc(archivo);
-                fscanf(archivo, " %d",&MT[i][j].dir);
+            fscanf(archivo,"%d",&MT[i][j].estado);
+            fseek(archivo, 1, SEEK_CUR);  // Ignora el espacio o salto de línea después del estado
+            fscanf(archivo,"%c",&MT[i][j].simbolo);
+            fseek(archivo, 1, SEEK_CUR);  // Ignora el espacio o salto de línea después del SIMBOLO
+            fscanf(archivo,"%d",&MT[i][j].dir);
+            fseek(archivo, 1, SEEK_CUR);  // Ignora el espacio o salto de línea después de la direccion
             }
         }
 
     }
     //Cerramos el archivo de texto
     fclose(archivo);
+    /* Solo veremos los caracteres de la cinta
 //Imprimir caracteres 
 
     printf("\n No. de estados: %d",No_estado);
@@ -91,37 +87,35 @@ int main(){
         }
         printf("\n\n");
     }
-
+    */
 //Recorremos la cinta para llenarlo con espacios en blanco
-    for(int i=0;i<max_cinta;i++){
-        cinta[i]=' '; //Tambien podemos representarlo como cinta[i]='\0'; \0 es para representar un espacio en blanco
+    for(int i=0;i<cinta_maxima;i++){
+        cinta[i]='_'; //Tambien podemos representarlo como cinta[i]='\0'; \0 es para representar un espacio en blanco
     }
-//El indice de la cinta apuntara a la mitad de esta
-    indice_cinta=max_cinta/2;
-//Tam_in es para saber el tamaño de la entrada del usuario
-tam_in = strlen(entrada);
-int i=0;
-//Recorremos la cinta para llenarlo con la entrada del usuario
+    //Tam_in es para saber el tamaño de la entrada del usuario
+    tam_in = strlen(entrada);
+    int i=0;
+    //Recorremos la cinta para llenarlo con la entrada del usuario
     for (int j=indice_cinta;j<indice_cinta+tam_in;j++){
         cinta[j]=entrada[i];
         i++;
     }
 
-//Declaramos una bandera para saber si la maquina de turing se detiene
-int bandera=0;
-//Y una variable auxiliar donde se guarda el estado inicial de la maquina de turing
-int aux_estado=estado_inicial;
-//Ahora recorremos la cinta con un do while mientras la bandera sea 0 y el estado no sea -1 si no se detiene
+    //Declaramos una bandera para saber si la maquina de turing se detiene
+    int bandera=0;
+    //Y una variable auxiliar donde se guarda el estado inicial de la maquina de turing
+    int aux_estado=estado_inicial;
+    //Ahora recorremos la cinta con un do while mientras la bandera sea 0 y el estado no sea -1 si no se detiene
+    int pos_simbolo=0;
     do{
-        int pos_simbolo=0;
+        //Retornamos la posicion de el simbolo en la matriz
         pos_simbolo=busqueda(cinta[indice_cinta]);
-        //Si el simobolo es diferente de -1 es decir un simbolo valido
+        //Si el simobolo es diferente de -1 es decir un simbolo existente
         if(pos_simbolo!=-1){
             //Entonces cambiamos los simbolos de la cinta
-            aux_estado=MT[aux_estado][pos_simbolo].estado;
             cinta[indice_cinta]=MT[aux_estado][pos_simbolo].simbolo;
-            //Y cambiamos la direccion de la cinta
             indice_cinta=indice_cinta+MT[aux_estado][pos_simbolo].dir;
+            aux_estado=MT[aux_estado][pos_simbolo].estado;
         }else{
             //Si el simbolo es -1 entonces la maquina de turing se detiene
             bandera=1;
@@ -133,7 +127,13 @@ int aux_estado=estado_inicial;
 
 //Ahora queda decir si fue aceptada la cadena o no
     if((estado_final!=0) && (aux_estado==estado_final)){
-        printf("\n La cadena fue aceptada\n");
+        printf("\nLa cadena fue aceptada\nCadena final\n");
+         for (int j=0;j<cinta_maxima;j++){
+            if(cinta[j]!='_'){
+            printf("[%c]",cinta[j]);
+            }
+        }
+        printf("\n");
     }else{
         printf("\n La cadena no fue aceptada\n");
     }
@@ -143,12 +143,12 @@ int aux_estado=estado_inicial;
 
 //Funcion para buscar el simbolo en la matriz de la maquina de turing
 int busqueda(char simbolo){ 
-    int pos_simbolo=-1;
-    for(int i=0;i<No_simbolo;i++){
-        if(MT[aux_estado][i].simbolo==simbolo){
-            pos_simbolo=i;
+    for(int i=0;i<No_simbolo;i++){ //Los simbolos totales de la cinta no pueden exeder a el numero de simbolos por lo que tenemos la posicion 
+        if(simbolo==Caracteres_cinta[i]){ //Si el simbolo enviado coincide con el de la cinta entonces
+            //Entonces regresamos la posicion del simbolo
+            return i;
         }
     }
-    return pos_simbolo;
+    return -1;
 
 }
